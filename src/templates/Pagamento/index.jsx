cf092ad6a872f49305462bb4090e909c522ce5a0
Iuri Santana <iuri.s.goes@gmail.com>
2022-06-11 22:02:22 -0300

    add front tela pagamento

--- src/templates/Pagamento/index.jsx
+++ src/templates/Pagamento/index.jsx
@@ -7,6 +7,7 @@ import './styles.css';
 import { ButtonUser } from '../../components/NavBar/ButtonUser';
 import LogoGreenPet from '../../assets/logo_greenpet.svg';
 import { ButtonCart } from '../../components/NavBar/ButtonCart';
+import CurrencyFormat from 'react-currency-format';
 
 export const Pagamento = () => {
 
@@ -30,6 +31,152 @@ export const Pagamento = () => {
           <Link to='/cart' className='button-cart'>
             <ButtonCart/>
           </Link>
+        </div>
+      </div>
+      <div className='main-container'>
+        <div className='container-endereco'>
+          <div className='titulo'>
+            <strong className='circulo-verde'>1</strong>
+            <span>Entrega</span>
+          </div>
+          <form>
+            <label>Nome completo<span>*</span></label>
+            <input 
+              id='nome-completo'
+              type='text'
+              name='nome'
+              placeholder='Ex.: José da Silva'
+            />
+            <label>CEP<span>*</span></label>
+            <CurrencyFormat
+              id='cep' 
+              name='cep' 
+              decimalSeparator=""
+              format="#####-###"
+              mask = "_"
+              allowNegative={false}
+              placeholder='Ex.: 00000-000'
+            />
+            <label>Endereço<span>*</span></label>
+            <input 
+              id='endereco'
+              type='text'
+              name='endereco'
+              placeholder='Ex.: Rua São José'
+            />
+            <div className='numero-bairro'>
+              <div>
+                <label>Número<span>*</span></label>
+                <input 
+                  id='n-residencial' 
+                  type='text'
+                  name='n-residencial' 
+                  placeholder='Ex.: 123'
+                />
+              </div>
+              <div>
+                <label>Bairro<span>*</span></label>
+                <input 
+                  id='bairro'
+                  type='text'
+                  name='bairro'
+                  placeholder='Ex.: Santo Amaro'
+                />
+              </div>
+            </div>
+            <label>CPF (Nota Fiscal)<span>*</span></label>
+            <CurrencyFormat
+              id='cpf-nota' 
+              name='cpf-nota' 
+              decimalSeparator=""
+              format="###.###.###-##"
+              mask = "_"
+              allowNegative={false}
+              placeholder='Ex.: 000.000.000-00'
+            />
+          </form>
+          <button className='button-continuar'>
+            Continuar
+          </button>
+        </div>
+        <div className='container-pagamento'>
+          <div className='titulo'>
+            <strong className='circulo-verde'>2</strong>
+            <span>Pagamento</span>
+          </div>
+          <form>
+            <label>Número do cartão<span>*</span></label>
+            <CurrencyFormat
+              id='cartao' 
+              name='cartao' 
+              decimalSeparator=""
+              format="#### #### #### ####"
+              mask = "_"
+              allowNegative={false}
+              placeholder='Ex.: 1234 1234 1234 1234'
+            />
+            <div className='validade-cvv'>
+              <div>
+                <label>Validade<span>*</span></label>
+                <CurrencyFormat
+                  id='validade' 
+                  name='validade' 
+                  decimalSeparator=""
+                  format="##/##"
+                  mask = "_"
+                  allowNegative={false}
+                  placeholder='MM/YY'
+                />
+              </div>
+              <div>
+                <label>CVV<span>*</span></label>
+                <CurrencyFormat
+                  id='cvv' 
+                  name='cvv' 
+                  decimalSeparator=""
+                  format="###"
+                  mask = "_"
+                  allowNegative={false}
+                  placeholder='CVV'
+                />
+              </div>
+            </div>
+            <label>Nome do titular<span>*</span></label>
+            <input 
+              id='nome-titular'
+              type='text'
+              name='nome'
+              placeholder='Ex.: José da Silva'
+            />
+            <label>Nº de parcelas<span>*</span></label>
+            <select name="parcelas" id="parcelas" placeholder='--'>
+              <option value="1">1x</option>
+              <option value="2">2x</option>
+              <option value="3">3x</option>
+              <option value="4">4x</option>
+              <option value="5">5x</option>
+              <option value="6">6x</option>
+              <option value="7">7x</option>
+              <option value="8">8x</option>
+              <option value="9">9x</option>
+              <option value="10">10x</option>
+              <option value="11">11x</option>
+              <option value="12">12x</option>
+            </select>
+          </form>
+        </div>
+        <div>
+          <div className='container-resumo'>
+            <div className='titulo'>
+              <span>Resumo</span>
+            </div>
+          </div>
+          <button className='button-continuar'>
+            Comprar
+          </button>
+          <button className='button-voltar'>
+            Voltar
+          </button>
         </div>
       </div>
     </section>
